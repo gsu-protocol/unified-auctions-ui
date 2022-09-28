eec616d68e7e7b57fa7696b6c8e924391ae715b8
iamsalmandev <dev.iamsalman@gmail.com>
2022-09-28 12:02:05 +0500

    remove irelevent colletrals

--- core/src/constants/COLLATERALS.ts
+++ core/src/constants/COLLATERALS.ts
@@ -1,46 +1,46 @@
 import type { CollateralConfig } from '../types';
 
 const COLLATERALS: Record<string, CollateralConfig> = {
-    'AAVE-A': {
-        title: 'AAVE',
-        ilk: 'AAVE-A',
-        symbol: 'AAVE',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
-    'BAL-A': {
-        title: 'Balancer',
-        ilk: 'BAL-A',
-        symbol: 'BAL',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
-    'BAT-A': {
-        title: 'Basic Attention Token',
-        ilk: 'BAT-A',
-        symbol: 'BAT',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
-    'COMP-A': {
-        title: 'Compound',
-        ilk: 'COMP-A',
-        symbol: 'COMP',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
+    // 'AAVE-A': {
+    //     title: 'AAVE',
+    //     ilk: 'AAVE-A',
+    //     symbol: 'AAVE',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'BAL-A': {
+    //     title: 'Balancer',
+    //     ilk: 'BAL-A',
+    //     symbol: 'BAL',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'BAT-A': {
+    //     title: 'Basic Attention Token',
+    //     ilk: 'BAT-A',
+    //     symbol: 'BAT',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'COMP-A': {
+    //     title: 'Compound',
+    //     ilk: 'COMP-A',
+    //     symbol: 'COMP',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
     'ETH-A': {
         title: 'Ether',
         ilk: 'ETH-A',
@@ -71,66 +71,66 @@ const COLLATERALS: Record<string, CollateralConfig> = {
             route: [],
         },
     },
-    'GUSD-A': {
-        title: 'Gemini Dollar',
-        ilk: 'GUSD-A',
-        symbol: 'GUSD',
-        decimals: 2,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
-    'KNC-A': {
-        title: 'Kyber Network Crystal',
-        ilk: 'KNC-A',
-        symbol: 'KNC',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
-    'LINK-A': {
-        title: 'Chainlink',
-        ilk: 'LINK-A',
-        symbol: 'LINK',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
-    'LRC-A': {
-        title: 'Loopring',
-        ilk: 'LRC-A',
-        symbol: 'LRC',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
-    'MANA-A': {
-        title: 'Decentraland',
-        ilk: 'MANA-A',
-        symbol: 'MANA',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
-    'PAXUSD-A': {
-        title: 'Paxos Standard',
-        ilk: 'PAXUSD-A',
-        symbol: 'PAXUSD',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
+    // 'GUSD-A': {
+    //     title: 'Gemini Dollar',
+    //     ilk: 'GUSD-A',
+    //     symbol: 'GUSD',
+    //     decimals: 2,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'KNC-A': {
+    //     title: 'Kyber Network Crystal',
+    //     ilk: 'KNC-A',
+    //     symbol: 'KNC',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'LINK-A': {
+    //     title: 'Chainlink',
+    //     ilk: 'LINK-A',
+    //     symbol: 'LINK',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'LRC-A': {
+    //     title: 'Loopring',
+    //     ilk: 'LRC-A',
+    //     symbol: 'LRC',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'MANA-A': {
+    //     title: 'Decentraland',
+    //     ilk: 'MANA-A',
+    //     symbol: 'MANA',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'PAXUSD-A': {
+    //     title: 'Paxos Standard',
+    //     ilk: 'PAXUSD-A',
+    //     symbol: 'PAXUSD',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
     'RENBTC-A': {
         title: 'renBTC',
         ilk: 'RENBTC-A',
@@ -141,56 +141,56 @@ const COLLATERALS: Record<string, CollateralConfig> = {
             route: ['ETH'],
         },
     },
-    'TUSD-A': {
-        title: 'True USD',
-        ilk: 'TUSD-A',
-        symbol: 'TUSD',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
-    'UNI-A': {
-        title: 'Uniswap',
-        ilk: 'UNI-A',
-        symbol: 'UNI',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
-    'USDC-A': {
-        title: 'USD Coin',
-        ilk: 'USDC-A',
-        symbol: 'USDC',
-        decimals: 6,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
-    'USDC-B': {
-        title: 'USD Coin',
-        ilk: 'USDC-B',
-        symbol: 'USDC',
-        decimals: 6,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
-    'USDT-A': {
-        title: 'Tether USD',
-        ilk: 'USDT-A',
-        symbol: 'USDT',
-        decimals: 6,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
+    // 'TUSD-A': {
+    //     title: 'True USD',
+    //     ilk: 'TUSD-A',
+    //     symbol: 'TUSD',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'UNI-A': {
+    //     title: 'Uniswap',
+    //     ilk: 'UNI-A',
+    //     symbol: 'UNI',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'USDC-A': {
+    //     title: 'USD Coin',
+    //     ilk: 'USDC-A',
+    //     symbol: 'USDC',
+    //     decimals: 6,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'USDC-B': {
+    //     title: 'USD Coin',
+    //     ilk: 'USDC-B',
+    //     symbol: 'USDC',
+    //     decimals: 6,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'USDT-A': {
+    //     title: 'Tether USD',
+    //     ilk: 'USDT-A',
+    //     symbol: 'USDT',
+    //     decimals: 6,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
     'WBTC-A': {
         title: 'Wrapped Bitcoin',
         ilk: 'WBTC-A',
@@ -221,36 +221,36 @@ const COLLATERALS: Record<string, CollateralConfig> = {
             route: ['ETH'],
         },
     },
-    'YFI-A': {
-        title: 'yearn.finance',
-        ilk: 'YFI-A',
-        symbol: 'YFI',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
-    'ZRX-A': {
-        title: '0x',
-        ilk: 'ZRX-A',
-        symbol: 'ZRX',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
-    'MATIC-A': {
-        title: 'Matic',
-        ilk: 'MATIC-A',
-        symbol: 'MATIC',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV3Callee',
-            route: ['ETH'],
-        },
-    },
+    // 'YFI-A': {
+    //     title: 'yearn.finance',
+    //     ilk: 'YFI-A',
+    //     symbol: 'YFI',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'ZRX-A': {
+    //     title: '0x',
+    //     ilk: 'ZRX-A',
+    //     symbol: 'ZRX',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'MATIC-A': {
+    //     title: 'Matic',
+    //     ilk: 'MATIC-A',
+    //     symbol: 'MATIC',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
     'WSTETH-A': {
         title: 'Lido wstETH',
         ilk: 'WSTETH-A',
@@ -271,126 +271,126 @@ const COLLATERALS: Record<string, CollateralConfig> = {
             route: [],
         },
     },
-    'CRVV1ETHSTETH-A': {
-        title: 'Curve stETH',
-        ilk: 'CRVV1ETHSTETH-A',
-        symbol: 'CRVV1ETHSTETH',
-        decimals: 18,
-        exchange: {
-            callee: 'CurveLpTokenUniv3Callee',
-            route: ['ETH'],
-        },
-    },
-    'UNIV2DAIETH-A': {
-        title: 'UNIV2DAIETH LP',
-        ilk: 'UNIV2DAIETH-A',
-        symbol: 'UNIV2DAIETH',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV2LpTokenCalleeDai',
-            token0: 'DAI',
-            token1: 'ETH',
-        },
-    },
-    'UNIV2USDCETH-A': {
-        title: 'UNIV2USDCETH LP',
-        ilk: 'UNIV2USDCETH-A',
-        symbol: 'UNIV2USDCETH',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV2LpTokenCalleeDai',
-            token0: 'USDC',
-            token1: 'ETH',
-        },
-    },
-    'UNIV2ETHUSDT-A': {
-        title: 'UNIV2ETHUSDT LP',
-        ilk: 'UNIV2ETHUSDT-A',
-        symbol: 'UNIV2ETHUSDT',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV2LpTokenCalleeDai',
-            token0: 'ETH',
-            token1: 'USDT',
-        },
-    },
-    'UNIV2WBTCDAI-A': {
-        title: 'UNIV2WBTCDAI LP',
-        ilk: 'UNIV2WBTCDAI-A',
-        symbol: 'UNIV2WBTCDAI',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV2LpTokenCalleeDai',
-            token0: 'WBTC',
-            token1: 'DAI',
-        },
-    },
-    'UNIV2WBTCETH-A': {
-        title: 'UNIV2WBTCETH LP',
-        ilk: 'UNIV2WBTCETH-A',
-        symbol: 'UNIV2WBTCETH',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV2LpTokenCalleeDai',
-            token0: 'WBTC',
-            token1: 'ETH',
-        },
-    },
-    'UNIV2LINKETH-A': {
-        title: 'UNIV2LINKETH LP',
-        ilk: 'UNIV2LINKETH-A',
-        symbol: 'UNIV2LINKETH',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV2LpTokenCalleeDai',
-            token0: 'LINK',
-            token1: 'ETH',
-        },
-    },
-    'UNIV2UNIETH-A': {
-        title: 'UNIV2UNIETH LP',
-        ilk: 'UNIV2UNIETH-A',
-        symbol: 'UNIV2UNIETH',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV2LpTokenCalleeDai',
-            token0: 'UNI',
-            token1: 'ETH',
-        },
-    },
-    'UNIV2AAVEETH-A': {
-        title: 'UNIV2AAVEETH LP',
-        ilk: 'UNIV2AAVEETH-A',
-        symbol: 'UNIV2AAVEETH',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV2LpTokenCalleeDai',
-            token0: 'AAVE',
-            token1: 'ETH',
-        },
-    },
-    'UNIV2DAIUSDT-A': {
-        title: 'UNIV2DAIUSDT LP',
-        ilk: 'UNIV2DAIUSDT-A',
-        symbol: 'UNIV2DAIUSDT',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV2LpTokenCalleeDai',
-            token0: 'DAI',
-            token1: 'USDT',
-        },
-    },
-    'UNIV2DAIUSDC-A': {
-        title: 'UNIV2DAIUSDC LP',
-        ilk: 'UNIV2DAIUSDC-A',
-        symbol: 'UNIV2DAIUSDC',
-        decimals: 18,
-        exchange: {
-            callee: 'UniswapV2LpTokenCalleeDai',
-            token0: 'DAI',
-            token1: 'USDC',
-        },
-    },
+    // 'CRVV1ETHSTETH-A': {
+    //     title: 'Curve stETH',
+    //     ilk: 'CRVV1ETHSTETH-A',
+    //     symbol: 'CRVV1ETHSTETH',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'CurveLpTokenUniv3Callee',
+    //         route: ['ETH'],
+    //     },
+    // },
+    // 'UNIV2DAIETH-A': {
+    //     title: 'UNIV2DAIETH LP',
+    //     ilk: 'UNIV2DAIETH-A',
+    //     symbol: 'UNIV2DAIETH',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV2LpTokenCalleeDai',
+    //         token0: 'DAI',
+    //         token1: 'ETH',
+    //     },
+    // },
+    // 'UNIV2USDCETH-A': {
+    //     title: 'UNIV2USDCETH LP',
+    //     ilk: 'UNIV2USDCETH-A',
+    //     symbol: 'UNIV2USDCETH',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV2LpTokenCalleeDai',
+    //         token0: 'USDC',
+    //         token1: 'ETH',
+    //     },
+    // },
+    // 'UNIV2ETHUSDT-A': {
+    //     title: 'UNIV2ETHUSDT LP',
+    //     ilk: 'UNIV2ETHUSDT-A',
+    //     symbol: 'UNIV2ETHUSDT',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV2LpTokenCalleeDai',
+    //         token0: 'ETH',
+    //         token1: 'USDT',
+    //     },
+    // },
+    // 'UNIV2WBTCDAI-A': {
+    //     title: 'UNIV2WBTCDAI LP',
+    //     ilk: 'UNIV2WBTCDAI-A',
+    //     symbol: 'UNIV2WBTCDAI',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV2LpTokenCalleeDai',
+    //         token0: 'WBTC',
+    //         token1: 'DAI',
+    //     },
+    // },
+    // 'UNIV2WBTCETH-A': {
+    //     title: 'UNIV2WBTCETH LP',
+    //     ilk: 'UNIV2WBTCETH-A',
+    //     symbol: 'UNIV2WBTCETH',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV2LpTokenCalleeDai',
+    //         token0: 'WBTC',
+    //         token1: 'ETH',
+    //     },
+    // },
+    // 'UNIV2LINKETH-A': {
+    //     title: 'UNIV2LINKETH LP',
+    //     ilk: 'UNIV2LINKETH-A',
+    //     symbol: 'UNIV2LINKETH',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV2LpTokenCalleeDai',
+    //         token0: 'LINK',
+    //         token1: 'ETH',
+    //     },
+    // },
+    // 'UNIV2UNIETH-A': {
+    //     title: 'UNIV2UNIETH LP',
+    //     ilk: 'UNIV2UNIETH-A',
+    //     symbol: 'UNIV2UNIETH',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV2LpTokenCalleeDai',
+    //         token0: 'UNI',
+    //         token1: 'ETH',
+    //     },
+    // },
+    // 'UNIV2AAVEETH-A': {
+    //     title: 'UNIV2AAVEETH LP',
+    //     ilk: 'UNIV2AAVEETH-A',
+    //     symbol: 'UNIV2AAVEETH',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV2LpTokenCalleeDai',
+    //         token0: 'AAVE',
+    //         token1: 'ETH',
+    //     },
+    // },
+    // 'UNIV2DAIUSDT-A': {
+    //     title: 'UNIV2DAIUSDT LP',
+    //     ilk: 'UNIV2DAIUSDT-A',
+    //     symbol: 'UNIV2DAIUSDT',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV2LpTokenCalleeDai',
+    //         token0: 'DAI',
+    //         token1: 'USDT',
+    //     },
+    // },
+    // 'UNIV2DAIUSDC-A': {
+    //     title: 'UNIV2DAIUSDC LP',
+    //     ilk: 'UNIV2DAIUSDC-A',
+    //     symbol: 'UNIV2DAIUSDC',
+    //     decimals: 18,
+    //     exchange: {
+    //         callee: 'UniswapV2LpTokenCalleeDai',
+    //         token0: 'DAI',
+    //         token1: 'USDC',
+    //     },
+    // },
 };
 
 export const getCollateralConfigBySymbol = function (symbol: string): CollateralConfig {
