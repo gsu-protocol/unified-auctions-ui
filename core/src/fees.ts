a8aec6eadccf9cb8afe4e8aa4f1ac6cb50b9043e
yghazi <yumnaghazi@gmail.com>
2022-10-14 18:59:32 +0500

    make auctions work

--- core/src/fees.ts
+++ core/src/fees.ts
@@ -6,7 +6,9 @@ import getSigner from './signer';
 import { getCollateralAuthorizationStatus, getWalletAuthorizationStatus } from './authorizations';
 
 export const convertETHtoDAI = async function (network: string, eth: BigNumber): Promise<BigNumber> {
-    const exchangeRate = await getMarketPrice(network, 'ETH');
+    // const exchangeRate = await getMarketPrice(network, 'ETH');
+    let exchangeRate = new BigNumber(1);
+    console.log("TODO: Need to fix rate calculation");
     return eth.multipliedBy(exchangeRate);
 };
 
--- frontend/lib/wallets/MetaMask.ts
+++ frontend/lib/wallets/MetaMask.ts
@@ -76,9 +76,9 @@ export default class MetaMask extends AbstractWallet {
     public async networkChangedHandler() {
         const networkType = getNetworkTypeByChainId(window.ethereum.chainId);
         const signer = await this.getSigner();
-        if (networkType) {
-            await setSigner(networkType, signer as any);
-        }
+        console.log('TODO: fixed so Metamask would connect with custom network.');
+        await setSigner(networkType || 'custom', signer as any);
+        
         window.$nuxt.$store.dispatch('network/setWalletChainId', window.ethereum.chainId);
     }
 
