5dcec19f4db7d67b233d89fb9fb3ff8fa3a38409
yghazi <yumnaghazi@gmail.com>
2022-11-03 19:17:31 +0500

    getMarketPrice fix in fees

--- core/src/fees.ts
+++ core/src/fees.ts
@@ -7,8 +7,6 @@ import { getCollateralAuthorizationStatus, getWalletAuthorizationStatus } from '
 
 export const convertETHtoDAI = async function (network: string, eth: BigNumber): Promise<BigNumber> {
     const exchangeRate = await getMarketPrice(network, 'ETH');
-    // let exchangeRate = new BigNumber(1);
-    console.log("TODO: Need to fix rate calculation", network);
     return eth.multipliedBy(exchangeRate);
 };
 
